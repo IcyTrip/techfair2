2c1b27bbd3e9751cc97d9ff66175a07ae91a2fb7
Daniel Sagan <dankasagan@gmail.com>
2025-12-05 11:42:06 -0700

    fadshgshfadfs

---
+++ contact.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Contact | SCD</title>
+        <link rel="stylesheet" href="contact.css">
+        <link rel="shortcut icon" href="images/Logo.png" type="image/x-icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <a class="logoA" href="index.html">
+                <img src="images/Logo.png" alt="SCD Logo" class="logo">
+                <h1>Specialized Concrete Designers</h1>
+            </a>
+            <div>
+                <div class="list">
+                    <a href="index.html">Home</a>
+                    <a href="sevices.html">Services</a>
+                    <a href="contact.html" class="buttonThing">Contact</a>
+                </div>
+            </div>
+        </header>
+        <main>
+            <section id="php-confirm">
+                <h3>
+                    Thank you for contacting Specialized Concrete Designers. We have received your message and will get back to you shortly.
+                </h3>
+            </section>
+        </main>
+        <footer>
+            <div class="footer-text">
+                &copy; 2025 SCD. All rights reserved.
+            </div>
+        </footer>
+    </body>
+</html>
